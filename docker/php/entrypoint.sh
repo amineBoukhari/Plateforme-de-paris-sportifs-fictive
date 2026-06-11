a091695d5040c031473e0c6dac05984779e5b378
amineBoukhari <amine.boukhari@tsplus.net>
2026-06-11 17:11:18 +0200

    update app secret

--- docker/php/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -e
-
-# Run migrations automatically on startup
-if [ "${APP_ENV}" = "prod" ]; then
-    php bin/console doctrine:migrations:migrate --no-interaction --allow-no-migration
-fi
-
-exec "$@"
